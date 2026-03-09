d93610287e882eca2d053521e7e8043058c5b8d7
Mônica de Oliveira <mcarmojm@gmail.com>
2026-03-09 18:33:27 -0300

    Correção: adicionado import os

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import os 
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,7 +10,10 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
+    activity = discord.Game(name="Dando aulas na Kodland")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
     print(f'Estamos logados como {bot.user}')
+    
 
 @bot.command()
 async def hello(ctx):
@@ -24,7 +28,6 @@ async def on_member_join(member):
     channel = member.guild.system_channel
     if channel:
         await channel.send(f'Bem-vindo ao servidor, {member.mention}! 🎉')
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
