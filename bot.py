4ff7d2d9d6c823efb67ad93e5476077351a3e78c
Mônica de Oliveira <mcarmojm@gmail.com>
2026-03-09 18:06:28 -0300

    Primeiro commit: Bot Discord configurado com variáveis de ambiente

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Estamos logados como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Olá! eu sou um bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+@bot.event
+async def on_member_join(member):
+    channel = member.guild.system_channel
+    if channel:
+        await channel.send(f'Bem-vindo ao servidor, {member.mention}! 🎉')
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot.run(TOKEN)
